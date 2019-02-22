6952d84428740aedf388200908ffa46a73dffb17
DINO2 <dino.jeong@nexstreaming.com>
2019-02-22 17:44:06 +0900

    Make Cube

---
+++ MetalPractice03/MetalPractice03/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalPractice03
+//
+//  Created by DINO2 on 22/02/2019.
+//  Copyright © 2019 DINO2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
