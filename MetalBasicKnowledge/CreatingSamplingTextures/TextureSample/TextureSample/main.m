d56c93b230a4a5c17a7c8e614da90673596a6d85
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-02-25 18:21:28 +0900

    feat: Making the project about texture

---
+++ MetalBasicKnowledge/CreatingSamplingTextures/TextureSample/TextureSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TextureSample
+//
+//  Created by HanGyo Jeong on 2020/02/25.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
