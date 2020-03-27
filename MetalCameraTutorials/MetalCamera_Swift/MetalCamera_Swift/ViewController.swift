c14ccd3f22a75fd0d0cfc16aa137d5555cba2335
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-03-27 09:45:02 +0900

    feat: Make new Project about Camera Application using Metal API

---
+++ MetalCameraTutorials/MetalCamera_Swift/MetalCamera_Swift/ViewController.swift
@@ -0,0 +1,108 @@
+//
+//  ViewController.swift
+//  MetalCamera_Swift
+//
+//  Created by HanGyo Jeong on 2020/03/27.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /*
+         Initialise session
+         */
+        let captureSession = AVCaptureSession()
+        
+        /*
+         Request access to hardware
+         */
+        AVCaptureDevice.requestAccess(for: AVMediaType.video) { (granted: Bool) -> Void in
+            guard granted else {
+                //Report an error. we didn't get access to hardware
+                NSLog("Fail to access hardware resource")
+                return
+            }
+            //All good, Hardware Access granted
+        }
+        
+        guard let inputDevice = device(mediaType: AVMediaType.video, position: AVCaptureDevice.Position.front) else {
+            //Handle an error. we couldn't get hold of the requested hardware
+            NSLog("Can't get hold of the requested hardware");
+            return
+        }
+        
+        /*
+         Add input to session
+         */
+        var captureInput: AVCaptureDeviceInput!
+        do {
+            captureInput = try AVCaptureDeviceInput(device: inputDevice)
+        }
+        catch {
+            //Handle an error. Input device is not available
+            NSLog("Input device is not available")
+        }
+        
+        captureSession.beginConfiguration()
+        
+        guard captureSession.canAddInput(captureInput) else {
+            //Handle an error, Failed to add an input device
+            NSLog("Fail to add an input device")
+            return
+        }
+        
+        captureSession.addInput(captureInput)
+        
+        /*
+         Add output to session
+         */
+        let outputData = AVCaptureVideoDataOutput()
+        
+        //Setting the camera data format
+        outputData.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String :kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange]
+        
+        //Setting delegate that is going to receive each and every video frame
+        let captureSessionQueue = DispatchQueue(label: "CameraSessionQueue", attributes: [])
+        outputData.setSampleBufferDelegate(self, queue: captureSessionQueue)
+        
+        guard captureSession.canAddOutput(outputData) else {
+            //Handle an error, when failed to add an output device
+            NSLog("Fail to add an output device")
+            return
+        }
+        captureSession.addOutput(outputData)
+        
+        /*
+         Start session
+         */
+        captureSession.commitConfiguration()
+        captureSession.startRunning()
+    }
+
+    //Get a AVCaptureDevice instance representing the camera
+    func device(mediaType: AVMediaType, position: AVCaptureDevice.Position) -> AVCaptureDevice? {
+        guard let devices = AVCaptureDevice.DiscoverySession.init(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: mediaType, position: position).devices as? [AVCaptureDevice] else
+        {
+            return nil;
+        }
+        
+        if let index = devices.firstIndex(where: { $0.position == position })
+        {
+            return devices[index]
+        }
+        return nil
+    }
+    
+    //AVCaptureVideoDataOutputSampleBufferDelegate method
+    //output callback that will be called for every frame, passing tthe frame data in a CMSampleBuffer
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        NSLog("captureOutput")
+    }
+}
+
---
+++ MetalCameraTutorials/README.md
@@ -0,0 +1,12 @@
+# Metal Camera Tutorial
+
+## What is AVCaptureSession?
+*AVCaptureSession* is a class in *AVFoundation* framework
+<br>*AVCaptureSession* object to coordinate the flow of data from audio or video input devices to outputs
+
+## These Project work flow
+- **Initialise session** : Session being actual AVCaptureSession Instance
+- **Request access to hardware** : Get a *AVCaptureDevice* instance<br>This instance represents a single hardware piece
+- **Add input to session** : Input is a *AVCaptureDeviceInput* instance initialised with *AVCaptureDevice*
+- **Add output to session** : *AVCaptureVideoDataOutput* instance that need to configure with the format you wantt your data in
+- **Start the session** : Data will be streaming
